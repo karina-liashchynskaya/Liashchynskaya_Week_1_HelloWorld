bf91b10f12577f604770cba433d67fe800ea9c30
NLias <NLias@LAPTOP-HGRESK7G>
2021-08-29 16:30:32 -0400

    Add project files.

---
+++ Liashchynskaya_Week_1_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Liashchynskaya_Week_1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
